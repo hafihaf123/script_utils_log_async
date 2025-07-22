4e6d33ae84206049dbff02655412129fa8b1044c
Michal Buchman <misobuchta@gmail.com>
2025-07-22 16:31:08 +0200

    update setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 _ = setup(
-    name="script_utils",
+    name="script_utils_log_async",
     version="0.1.0",
     description="Minimal shared utilities for logging and main wrapping",
     author="Michal Buchman",
-    author_email="xbuchmm00@stud.fit.vut.cz",
+    author_email="misobuchta@gmail.com",
     license="MIT",
-    packages=["script_utils"],
+    packages=["script_utils_log_async"],
     zip_safe=False,
 )
