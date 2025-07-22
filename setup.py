e4b400b74e8f94740b8d930b65e9867ba7cc6d4f
Michal Buchman <misobuchta@gmail.com>
2025-07-22 16:17:00 +0200

    add pyproject.toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 _ = setup(
     name="script_utils",
-    version="0.1",
+    version="0.1.0",
     description="Minimal shared utilities for logging and main wrapping",
     author="Michal Buchman",
     author_email="xbuchmm00@stud.fit.vut.cz",
