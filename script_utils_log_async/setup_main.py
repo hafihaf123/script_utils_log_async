d3e81fef557cdc7192e1596584ed827884615cea
Michal Buchman <misobuchta@gmail.com>
2025-07-23 14:06:25 +0200

    0.2.0 alpha - add config class, more things to configure

--- script_utils_log_async/setup_main.py
+++ script_utils_log_async/setup_main.py
@@ -1,9 +1,10 @@
 import asyncio
+from dataclasses import dataclass
 import logging
 import signal
 import time
 from collections.abc import Coroutine
-from typing import Callable, Literal, Union, cast, overload
+from typing import Callable, Optional, Union, cast
 
 from script_utils_log_async.logging_setup import setup_logging, trigger_shutdown_filter
 
@@ -13,35 +14,58 @@ SyncMainCoroutine = VoidFun
 MainCoroutine = Callable[..., Union[Coroutine[object, object, None], None]]
 
 
-def my_start_end_decorator(log_time: bool, is_async: bool):
+@dataclass
+class SetupMainConfig:
+    @staticmethod
+    def my_on_main_start():
+        print("=" * 70)
+        logging.info("Script started.")
+        print("=" * 70)
+
+    @staticmethod
+    def my_on_shutdown_catch():
+        logging.warning("Shutdown signal received. Shutting down.")
+        trigger_shutdown_filter()
+
+    is_async: bool
+    log_time: bool = True
+    on_main_start: VoidFun = my_on_main_start
+    on_main_exception: VoidFun = lambda: logging.exception(
+        "Unhandled exception occurred in main."
+    )
+    on_shutdown_catch: VoidFun = my_on_shutdown_catch
+    on_cancel: VoidFun = lambda: logging.info("Currently running task was cancelled.")
+    on_finish: VoidFun = lambda: logging.info("Current task completed normally.")
+    setup_logging: VoidFun = setup_logging
+
+
+def start_end_decorator(config: SetupMainConfig):
     def decorator(func: MainCoroutine) -> Union[AsyncMainCoroutine, SyncMainCoroutine]:
         async def async_wrapper():
-            print("=" * 70)
-            logging.info("Script started.")
-            print("=" * 70)
+            config.on_main_start()
             start_time = None
-            if log_time:
+            if config.log_time:
                 start_time = time.time()
 
             try:
-                if is_async:
+                if config.is_async:
                     main = cast(AsyncMainCoroutine, func)
                     await main()
                 else:
                     main = cast(SyncMainCoroutine, func)
                     main()
             except Exception:
-                logging.exception("Unhandled exception occurred in main.")
+                config.on_main_exception()
             finally:
-                if log_time and start_time:
+                if config.log_time and start_time:
                     end_time = round(time.time() - start_time, 2)
-                    logging.info(f"Program exiting after {end_time} seconds.")
+                    logging.info(f"Script ended after {end_time} seconds.")
                 print()
 
         def sync_wrapper():
             asyncio.run(async_wrapper())
 
-        if is_async:
+        if config.is_async:
             return async_wrapper
         else:
             return sync_wrapper
@@ -49,67 +73,32 @@ def my_start_end_decorator(log_time: bool, is_async: bool):
     return decorator
 
 
-def my_on_shutdown_catch():
-    logging.warning("Shutdown signal received! Saving prograss and shutting down.")
-    trigger_shutdown_filter()
-
-
-@overload
 def setup_main(
-    *,
-    is_async: Literal[True],
-    log_time: bool = ...,
-    start_end_decorator: Callable[
-        [bool, bool], Callable[[MainCoroutine], SyncMainCoroutine]
-    ] = ...,
-    setup_logging_func: VoidFun = ...,
-    on_shutdown_catch: VoidFun = ...,
-    on_cancel: VoidFun = ...,
-    on_finish: VoidFun = ...,
-) -> Callable[[AsyncMainCoroutine], VoidFun]: ...
-@overload
-def setup_main(
-    *,
-    is_async: Literal[False],
-    log_time: bool = ...,
-    start_end_decorator: Callable[
-        [bool, bool], Callable[[MainCoroutine], AsyncMainCoroutine]
-    ] = ...,
-    setup_logging_func: VoidFun = ...,
-    on_shutdown_catch: VoidFun = ...,
-) -> Callable[[SyncMainCoroutine], VoidFun]: ...
-def setup_main(
-    *,
     is_async: bool,
-    log_time: bool = True,
-    start_end_decorator: Callable[
-        [bool, bool],
-        Callable[[MainCoroutine], Union[AsyncMainCoroutine, SyncMainCoroutine]],
-    ] = my_start_end_decorator,
-    setup_logging_func: VoidFun = setup_logging,
-    on_shutdown_catch: VoidFun = my_on_shutdown_catch,
-    on_cancel: VoidFun = lambda: logging.info("Currently running task was cancelled."),
-    on_finish: VoidFun = lambda: logging.info("Current task completed normally."),
-) -> Callable[..., Callable[[], Union[Coroutine[object, object, None], None]]]:
+    config: Optional[SetupMainConfig] = None,
+) -> Callable[[MainCoroutine], SyncMainCoroutine]:
+    if not config:
+        config = SetupMainConfig(is_async)
+
     def decorator(main_coroutine: MainCoroutine):
         async def async_wrapper():
-            setup_logging_func()
+            config.setup_logging()
 
             if is_async:
                 async_start_end_decorator = cast(
                     Callable[
-                        [bool, bool], Callable[[MainCoroutine], AsyncMainCoroutine]
+                        [SetupMainConfig], Callable[[MainCoroutine], AsyncMainCoroutine]
                     ],
                     start_end_decorator,
                 )
 
-                @async_start_end_decorator(log_time, is_async)
+                @async_start_end_decorator(config)
                 async def async_main():
                     loop = asyncio.get_running_loop()
                     stop_event = asyncio.Event()
 
                     def on_shutdown():
-                        on_shutdown_catch()
+                        config.on_shutdown_catch()
                         stop_event.set()
 
                     signals_to_handle: list[int] = [
@@ -132,15 +121,13 @@ def setup_main(
                         try:
                             await main_task
                         except asyncio.CancelledError:
-                            on_cancel()
+                            config.on_cancel()
                     else:
-                        on_finish()
+                        config.on_finish()
 
                 await async_main()
             else:
-                decorated_main_coroutine = start_end_decorator(log_time, is_async)(
-                    main_coroutine
-                )
+                decorated_main_coroutine = start_end_decorator(config)(main_coroutine)
                 sync_main_coroutine = cast(SyncMainCoroutine, decorated_main_coroutine)
                 sync_main_coroutine()
 
